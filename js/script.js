0b7ad940b0a4a50fda83a67180c4a2416fa3152f
Isabelle Toniolo <isabelletoniolom@gmail.com>
2023-03-14 17:27:30 -0300

    Ingresso e Informações
    Comecei as páginas ingresso e informações

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-const menuHamburguer = document.querySelector(".material-symbols-outlined")
-const menuNav = document.querySelector(".nav-menu")
+const menuHamburguer = document.querySelector(".material-symbols-outlined");
+const menuNav = document.querySelector(".nav-menu");
 
 function toggleMenu(){
     if(menuNav.classList.contains('show-menu')) {
         menuNav.classList.remove('show-menu');
     } else {
-        menuNav.classList.add('show-menu')
+        menuNav.classList.add('show-menu');
+    }
+}
+menuHamburguer.addEventListener("click", toggleMenu);
+
+
+/* TIRAR DUVIDA
+const setas = document.querySelector(".dropdown-icon");
+const respostas = document.querySelector(".resposta");
+
+function dropDown() {
+    if(respostas.classList.contains('resposta-dropped')) {
+       respostas.classList.remove('resposta-dropped');
+    } else {
+        this.classList.add('resposta-dropped');
     }
 }
 
-menuHamburguer.addEventListener("click", toggleMenu);
+setas.forEach((elemento) => {
+    elemento.addEventListener("click", dropDown);
+})
+*/
